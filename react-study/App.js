3f9a61f01f6ca2ef046c9a49d36b8da856018262
yiyan12138 <yangyi.dfl@gmail.com>
2026-05-15 07:59:58 +0200

    add react projet

--- react-study/App.js
+++ react-study/App.js
@@ -13,24 +13,28 @@ function MyButton(){
 //     );
 
 // }
-export default function Square() {
+function Square({value}){
+    return <button className="square">{value}</button>
+}
+export default function Board() {
     return ( 
         <>
             <div className="board-row">
-                <button className="square">1</button>
-                <button className="square">1</button>
-                <button className="square">1</button>
+                <Square value="1"/>
+                <Square value="2"/>
+                <Square value="3"/>
             </div>
             <div className="board-row">
-                <button className="square">1</button>
-                <button className="square">1</button>
-                <button className="square">1</button>
+                <Square value="4"/>
+                <Square value="5"/>
+                <Square value="6"/>
             </div>
             <div className="board-row">
-                <button className="square">1</button>
-                <button className="square">1</button>
-                <button className="square">1</button>
+                <Square value="7"/>
+                <Square value="8"/>
+                <Square value="9"/>
             </div>
                 
-        </>);
+        </>
+    );
 }
